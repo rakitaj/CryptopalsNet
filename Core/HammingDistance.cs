9706e1fd2ec7da916c24f3a0a774a883e677d407
Joshua Rakita <joshuarakita@gmail.com>
2019-12-29 23:23:47 -0800

    Implement Hamming Distance between byte arrays.

---
+++ Core/HammingDistance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptopalsNet.Core
+{
+    public class HammingDistance
+    {
+        public ByteArray ByteArray { get; }
+        public HammingDistance(ByteArray byteArray)
+        {
+            this.ByteArray = byteArray;
+        }
+
+        public int Against(ByteArray other)
+        {
+            int count = 0;
+            var bitArray1 = new BitArray(this.ByteArray.Bytes.ToArray());
+            var bitArray2 = new BitArray(other.Bytes.ToArray());
+            var difference = bitArray1.Xor(bitArray2);
+            foreach(var bit in difference)
+            {
+                count += (bool)bit == true ? 1 : 0;
+            }
+            return count;
+        }
+    }
+}
---
+++ CoreTests/HammingDistanceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CryptopalsNet.Core;
+using NUnit.Framework;
+
+namespace CoreTests
+{
+    public class HammingDistanceTests
+    {
+        [Test]
+        public void HammingDistance_Against_CryptopalsExample()
+        {
+            var first = ByteArray.FromAscii("this is a test");
+            var second = ByteArray.FromAscii("wokka wokka!!!");
+            var hammingDistance = new HammingDistance(first);
+            Assert.AreEqual(37, hammingDistance.Against(second));
+        }
+    }
+}
